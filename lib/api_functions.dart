9597a033b09da89bfaca7daa2acd7ca82f6b3f8a
caveinn <caveinncicad@gmail.com>
2020-01-15 23:30:11 +0300

    add top movies

--- lib/api_functions.dart
+++ lib/api_functions.dart
@@ -4,19 +4,41 @@ import 'package:movie_info/models.dart';
 import 'package:http/http.dart' as http;
 
 Future<List<TvShow>> getTopTvShows() async {
-    List<TvShow> popularTvshows = [];
-    http.Response response = await http.get(
-        'https://api.themoviedb.org/3/tv/popular?api_key='+ DotEnv().env['Auth3token']+'&language=en-US&page=1');
-    var jsonData = jsonDecode(response.body);
-    for (var Tv in jsonData["results"]) {
-      TvShow show = TvShow(
-          name: Tv["name"],
-          backdropPath: Tv["backdrop_path"],
-          overview: Tv['overview'],
-          posterpath: Tv['poster_path'],
-          voteAvarage: Tv['vote_avarage'],
-          id: Tv['id']);
-      popularTvshows.add(show);
-    }
-    return popularTvshows;
+  List<TvShow> popularTvshows = [];
+  http.Response response = await http.get(
+      'https://api.themoviedb.org/3/tv/popular?api_key=' +
+          DotEnv().env['Auth3token'] +
+          '&language=en-US&page=1');
+  var jsonData = jsonDecode(response.body);
+  for (var Tv in jsonData["results"]) {
+    TvShow show = TvShow(
+        name: Tv["name"],
+        backdropPath: Tv["backdrop_path"],
+        overview: Tv['overview'],
+        posterpath: Tv['poster_path'],
+        voteAvarage: Tv['vote_avarage'],
+        id: Tv['id']);
+    popularTvshows.add(show);
   }
+  return popularTvshows;
+}
+
+Future<List<TvShow>> getTopMovies() async {
+  List<TvShow> popularTvshows = [];
+  http.Response response = await http.get(
+      'https://api.themoviedb.org/3/movie/top_rated?api_key=' +
+          DotEnv().env['Auth3token'] +
+          '&language=en-US&page=1');
+  var jsonData = jsonDecode(response.body);
+  for (var Tv in jsonData["results"]) {
+    TvShow show = TvShow(
+        name: Tv["name"],
+        backdropPath: Tv["backdrop_path"],
+        overview: Tv['overview'],
+        posterpath: Tv['poster_path'],
+        voteAvarage: Tv['vote_avarage'],
+        id: Tv['id']);
+    popularTvshows.add(show);
+  }
+  return popularTvshows;
+}
--- lib/models.dart
+++ lib/models.dart
@@ -1,16 +1,34 @@
 class TvShow {
-  String name;
+  String title;
   int id;
   String backdropPath;
   String overview;
-  String posterpath;
+  String posterPath;
   String voteAvarage;
 
   TvShow(
-      {this.name,
+      {this.title,
       this.backdropPath,
       this.overview,
-      this.posterpath,
+      this.posterPath,
+      this.voteAvarage,
+      this.id});
+
+}
+
+class Movie {
+  String title;
+  int id;
+  String backdropPath;
+  String overview;
+  String posterPath;
+  String voteAvarage;
+
+  Movie(
+      {this.title,
+      this.backdropPath,
+      this.overview,
+      this.posterPath,
       this.voteAvarage,
       this.id});
 
